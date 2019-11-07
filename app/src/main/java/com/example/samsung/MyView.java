d36ecfe1ce4ac24900836a9771d827da6e66ae7b
cypermen <andreybabich2012@yandex.ru>
2019-11-08 00:35:43 +0300

    1.8.8 function charts

--- app/src/main/java/com/example/samsung/MyView.java
+++ app/src/main/java/com/example/samsung/MyView.java
@@ -4,38 +4,67 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Shader;
 import android.view.View;
 
 public class MyView extends View {
-    Paint paint = new Paint();
-    int N = 10;
-    float[] x  = new float[N];
-    float[] y  = new float[N];
-    float[] vx = new float[N];
-    float[] vy = new float[N];
+    double xe, ye, w;
+    double x, y, h = 0.1;
+    int k = 100;
+    double xmin = -4, xmax = 4;
+    @Override
+    protected void onDraw(Canvas canvas) {
 
-    public MyView(Context context) {
-        super(context);
-        for (int i = 0; i < N; i++){
-            x[i] = (float)(Math.random() * 500);
-            y[i] = (float)(Math.random() * 500);
-            vx[i] = (float)(Math.random() * 6 - 3);
-            vy[i] = (float)(Math.random() * 6 - 3);
+        int x0 = this.getWidth() / 2;
+        int y0 = this.getHeight() / 2;
+
+        Paint paint = new Paint();
+        paint.setStrokeWidth(5);
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setTextSize(30.0f);
+
+        canvas.drawLine(x0, 0, x0, this.getHeight(), paint);
+        canvas.drawText("Y", this.getWidth() / 2 + 10, 35, paint);
+        canvas.drawLine(0, y0, this.getWidth(), y0, paint);
+        canvas.drawText("X", this.getWidth() - 50, this.getHeight() / 2 + 50, paint);
+
+        Path path = new Path();
+        boolean first = true;
+        paint.setColor(Color.BLUE);
+        for (x = xmin; x < xmax; x += h) {
+
+            y=Math.abs(x);
+            xe = x0 + k * x;
+            ye = y0 - k * y;
+            if (first) {
+                path.moveTo((float) xe, (float) ye);
+                first = false;
+            } else {
+                path.lineTo((float) xe, (float) ye);
+            }
+            canvas.drawPath(path, paint);
+        }
+        Path path2 = new Path();
+        first = true;
+        paint.setColor(Color.RED);
+        for (x = xmin; x < xmax; x += h) {
+            y=-x*x+3;
+            xe = x0 + k * x;
+            ye = y0 - k * y;
+            if (first) {
+                path2.moveTo((float) xe, (float) ye);
+                first = false;
+            }
+            else
+            {
+                path2.lineTo((float) xe, (float) ye);
+            }
+            canvas.drawPath(path2, paint);
         }
     }
-
-
-    @Override
-    protected void onDraw(Canvas canvas){
-        for (int i = 0; i < N; i++) {
-            canvas.drawCircle(x[i], y[i], 20, paint);
-        }
-
-        for (int i = 0; i < N; i++) {
-            x[i] += vx[i];
-            y[i] += vy[i];
-        }
-
-        invalidate();
+    public MyView(Context context) {
+        super(context);
     }
 }
