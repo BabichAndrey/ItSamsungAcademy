f9b096fdff1cf693f1c601818bf8254d930822c5
cypermen <andreybabich2012@yandex.ru>
2019-11-07 22:34:08 +0300

    1.7.6  drawing patterns on canvas
    Green thick lines

--- app/src/main/java/com/example/samsung/MyView.java
+++ app/src/main/java/com/example/samsung/MyView.java
@@ -2,6 +2,7 @@ package com.example.samsung;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 
@@ -15,11 +16,9 @@ public class MyView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Paint paint = new Paint();
-        int y = 0;
-        while (y < canvas.getHeight()) {
-            canvas.drawLine(0, y,
-                    this.getWidth(), y, paint);
-            y += 30;
-        }
+        paint.setColor(Color.GREEN);
+        paint.setStrokeWidth(5);
+        canvas.drawLine(0,0,canvas.getWidth(),canvas.getHeight(),paint);
+        canvas.drawLine(canvas.getWidth(),0,0,canvas.getHeight(),paint);
     }
 }
